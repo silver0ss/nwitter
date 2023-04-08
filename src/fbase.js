0edaf309538b7d1b344656d653b15f9ad351308e
silver0ss <silver0ss@naver.com>
2023-04-08 21:56:03 +0900

    2.0 Setting Auth

--- src/fbase.js
+++ src/fbase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +15,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
+const fbase = initializeApp(firebaseConfig);
+
+export const authService = getAuth(fbase);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import { firebaseApp } from "./firebase";
-console.log(firebaseApp);
+import App from "components/App";
+import { authService } from "fbase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
