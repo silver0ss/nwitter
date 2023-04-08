3960e422dde0a68ff758e4a17b87be23076a361a
silver0ss <silver0ss@naver.com>
2023-04-08 20:40:13 +0900

    1.2 Router Setup

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAjHg3QfUEv8H63593b57PY06Rdw_mxcys",
-  authDomain: "nwitter-d7bb3.firebaseapp.com",
-  projectId: "nwitter-d7bb3",
-  storageBucket: "nwitter-d7bb3.appspot.com",
-  messagingSenderId: "833797631860",
-  appId: "1:833797631860:web:79bd5351ceab27fbff0621",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
-export const FirebaseApp = initializeApp(firebaseConfig);
+export const firebaseApp = initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/App";
+import { firebaseApp } from "./firebase";
+console.log(firebaseApp);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
